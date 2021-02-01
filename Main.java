075e6b4883142a93c535f222b3961d9ad2a360ef
lmonteagudo15 <lyanna.monteagudo@patelhs.org>
2021-02-01 13:13:48 +0000

    i added a comment abt asking the user their age

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("How old are you");
+    int age = scan.nextInt();
+    // asking the user their age 
+    System.out.println("what is the current year");
+    int year = scan.nextInt();
+    
+
+    System.out.println(gradYear(age, year));
+  }
+
+  static int gradYear(int currentAge, int currentYear) {
+    int yearsLeft = 18 - currentAge;
+    return yearsLeft + currentYear;
   }
 }
