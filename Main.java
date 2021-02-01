8810b2cb361ff27f8e7825dc22fcf6998d9868c8
lmonteagudo15 <lyanna.monteagudo@patelhs.org>
2021-02-01 14:05:09 +0000

    added a comment for the print statement

--- Main.java
+++ Main.java
@@ -6,22 +6,23 @@ class Main {
 
     System.out.println("How old are you");
     int age = scan.nextInt();
-    // asking the user their age 
+    // asking the user their age
     System.out.println("what is the current year");
     int year = scan.nextInt();
-    // asking the user the current year 
+    // asking the user the current year
     System.out.println("What school are you graduating from?");
     String school = scan.next();
 
-    System.out.println("You will graduate in "+ gradYear(age, year));
+    System.out.println("You will graduate in " + gradYear(age, year));
+    // prints out the year you will graduate
   }
 
   static int gradYear(int currentAge, int currentYear) {
     int yearsLeft = 18 - currentAge;
     return yearsLeft + currentYear;
   }
-  static String school(String name){
-    String school = "";
 
+  static String school(String name) {
+    return "hello" + name;
   }
 }
